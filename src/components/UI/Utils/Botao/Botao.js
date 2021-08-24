a67d0e69b290fd66c2390c7cd6a7b7211cd0da95
Thiago Damaceno <tavaresdamaceno@gmail.com>
2021-08-24 20:31:31 -0300

    ADD: Dependencias do bootstrap e react icons, componente de botao e constantes css
    Task #15971

---
+++ src/components/UI/Utils/Botao/Botao.js
@@ -0,0 +1,19 @@
+import react from 'react';
+import './Botao.css';
+
+/**
+ * props = {
+ *  type,
+ *  onClick
+ * }
+ */
+export default (props) => {
+  const type = props.type ? props.type : 'button';
+  const className = 'botao ' + (props.className ? props.className : '');
+
+  return (
+    <button className={className} onClick={props.onClick} type={type}>
+      {props.children}
+    </button>
+  );
+}
