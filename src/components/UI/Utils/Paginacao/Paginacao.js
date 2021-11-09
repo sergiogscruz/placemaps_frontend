dda27c134da632203779e09d2570d9d69100a957
Sergio Gabriel <sergiogscruz@gmail.com>
2021-11-09 00:24:58 -0300

    CHG: deploy Heroku
    Issue: 16586

--- src/components/UI/Utils/Paginacao/Paginacao.js
+++ src/components/UI/Utils/Paginacao/Paginacao.js
@@ -20,9 +20,9 @@ export default function Paginacao(props) {
       setPaginaAtual(paginaAtual + 1)
   }
 
-  useEffect(() => {
-    //props.setStateOnChange(paginaAtual)
-  }, [paginaAtual, props]);
+  //useEffect(() => {
+  //  props.setStateOnChange(paginaAtual)
+  //}, [paginaAtual, props]);
 
 
   const montarElementos = () => {
