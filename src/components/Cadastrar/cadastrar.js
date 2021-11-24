52b78092cb93550fd01a8fe0f59a25f3d1812721
Keven Luan da Silva <ra-19138376-2@alunos.unicesumar.edu.br>
2021-11-24 02:39:26 -0300

    Adicionando foto ao firebase

--- src/components/Cadastrar/cadastrar.js
+++ src/components/Cadastrar/cadastrar.js
@@ -8,6 +8,7 @@ import { Formik, Form, Field } from 'formik';
 import { Redirect, Link } from 'react-router-dom';
 import axios from 'axios';
 import { AxiosHelper } from '../services/api';
+import { UploadImageFirabase } from '../Firebase/upload-image-firebase';
 
 export default function Cadastrar() {
   const [concordaTermos, setConcordaTermos] = useState(false);
@@ -15,6 +16,7 @@ export default function Cadastrar() {
   const [pendingRequest, setPendingRequest] = useState(false);
   const [tipoUsuario, setTipoUsuario] = useState('');
   const [redireciona, setRedireciona] = useState(false);
+  const [fotoPerfil, setFotoPerfil] = useState({url: 'https://firebasestorage.googleapis.com/v0/b/place-maps-331023.appspot.com/o/imagens%2Fno-user-image-icon-27.jpg?alt=media&token=55643b2f-4342-46c5-bb5e-1631b6070a07'});
 
   useEffect(() => {
     setRedireciona(true);
@@ -33,6 +35,16 @@ export default function Cadastrar() {
       confirmSenha: yup.string().min(6,'É necessário no mínimo 6 caracteres').oneOf([yup.ref('senha'), null], 'As senhas não são iguais'),
       concordaTermos: yup.boolean()
     })
+
+  const handleChange = async (event) => {
+    const image = event.target.files[0]
+    if (image) {
+      const url = await UploadImageFirabase.upload('perfil', image)
+      setTimeout(async () => {
+        setFotoPerfil({url: url})
+      }, 2000)
+    }
+  }
 
   return (
     <div className="pm-cadastrar-conteudo">
@@ -57,6 +69,7 @@ export default function Cadastrar() {
           validationSchema={validation}
           onSubmit={values => {
             setPendingRequest(true)
+            values['foto'] = fotoPerfil
             axios.post('api/public/autenticacao/cadastrar-usuario', values )
               .then((result) => {
               setPendingRequest(false)
@@ -95,6 +108,16 @@ export default function Cadastrar() {
                   <label>Confirmar Senha*</label>
                   <Field className="pm-cadastrar-input-largura field" name="confirmSenha" type="password" />
                   {errors.confirmSenha && touched.confirmSenha ? <div style={{color: "#EC1F46"}}>{errors.confirmSenha}</div> : null}
+                </div>
+                <div className="foto">
+                  <div>
+                    <img className="imagem" src={(fotoPerfil.url)} alt="firebase-image" style={{maxWidth: "150px", alignItems: "center"}}/>
+                  </div>
+                  <div>
+                    <label className="btn btn-primary">
+                      <i className="fa fa-image"></i> Escolher foto <input className="input-file" type="file" onChange={handleChange} accept="image/png, image/jpeg" />
+                    </label>
+                  </div>
                 </div>
                 <div className="pm-cadastrar-termos">
                   <Field type="checkbox" name="concordaTermos" onClick={() => {setConcordaTermos(!concordaTermos)}}/>
